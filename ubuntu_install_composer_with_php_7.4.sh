a0297215cab643365d002b53eca90d29ca788c58
ldumay <dumay.famille@gmail.com>
2023-03-08 23:00:02 +0100

    up

--- ubuntu_install_composer_with_php_7.4.sh
+++ ubuntu_install_composer_with_php_7.4.sh
@@ -60,7 +60,10 @@ php -v
 echo $separateLine
 echo "[Script - PHP & Composer - $phpVersion] Installation de Composer"
 echo $separateLine
-sudo apt install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
 echo $separateLine
 echo "[Script - PHP & Composer - $phpVersion] Installation de Composer terminée"
 echo $separateLine
