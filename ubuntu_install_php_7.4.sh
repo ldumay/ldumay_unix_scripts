61f20c01952983433f1570f1f71dc0d830210ff0
ldumay <dumay.famille@gmail.com>
2023-03-08 22:35:22 +0100

    Update ubuntu_install_php_7.4.sh

--- ubuntu_install_php_7.4.sh
+++ ubuntu_install_php_7.4.sh
@@ -1,9 +1,54 @@
-echo "[Script - PHP - 7.4] Installation de PHP 7.4 sur Ubuntu 22.04"
+phpVersion="7.4"
+echo "[Script - PHP - $phpVersion] Installation de PHP $phpVersion sur Ubuntu 22.04"
 echo "- - - - - - - - - - - - "
-echo "[Script - PHP - 7.4] Mis à jour des dépôts"
+echo "[Script - PHP - $phpVersion] Mise à jour des dépôts"
+echo "- - - - - - - - - - - - "
 sudo apt update && apt upgrade
-echo "[Script - PHP - 7.4] Ajout du dépôt de PHP 7.4"
+echo "[Script - PHP - $phpVersion] Ajout du dépôt de PHP $phpVersion"
+echo "- - - - - - - - - - - - "
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-echo "[Script - PHP - 7.4] Mise à jour des dépôts"
+echo "[Script - PHP - $phpVersion] Mise à jour des dépôts"
+echo "- - - - - - - - - - - - "
 sudo apt update && apt upgrade
+echo "[Script - PHP - $phpVersion] Installation des prérequis"
+echo "- - - - - - - - - - - - "
+sudo apt install curl wget git tree nano zip unzip libpcre3
+echo "[Script - PHP - $phpVersion] Installation de Composer"
+sudo apt install composer
+echo "[Script - PHP - $phpVersion] Nettoyage des précédentes versions de PHP"
+echo "- - - - - - - - - - - - "
+sudo apt-get purge 'php*'
+echo "[Script - PHP - $phpVersion] Installation de PHP $phpVersion"
+echo "- - - - - - - - - - - - "
+sudo apt -y install php$phpVersion
+sudo apt -y install php$phpVersion-amqp
+sudo apt -y install php$phpVersion-apcu
+sudo apt -y install php$phpVersion-bcmath
+sudo apt -y install php$phpVersion-cli
+sudo apt -y install php$phpVersion-common
+sudo apt -y install php$phpVersion-curl
+sudo apt -y install php$phpVersion-ctype
+sudo apt -y install php$phpVersion-dom
+sudo apt -y install php$phpVersion-fpm
+sudo apt -y install php$phpVersion-gd
+sudo apt -y install php$phpVersion-gmp
+sudo apt -y install php$phpVersion-intl
+sudo apt -y install php$phpVersion-json
+sudo apt -y install php$phpVersion-mbstring
+sudo apt -y install php$phpVersion-mysql 
+sudo apt -y install php$phpVersion-opcache
+sudo apt -y install php$phpVersion-pdo
+sudo apt -y install php$phpVersion-pgsql
+sudo apt -y install php$phpVersion-readline
+sudo apt -y install php$phpVersion-redis
+sudo apt -y install php$phpVersion-simplexml
+sudo apt -y install php$phpVersion-soap
+sudo apt -y install php$phpVersion-sqlite3
+sudo apt -y install php$phpVersion-tokenizer
+sudo apt -y install php$phpVersion-xdebug
+sudo apt -y install php$phpVersion-xml 
+sudo apt -y install php$phpVersion-zip
+echo "[Script - PHP - $phpVersion] Installation terminée"
+echo "- - - - - - - - - - - - "
+php -v
