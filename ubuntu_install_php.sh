c544fe4ba04e4b3a0ebdf8d4d4f83606fbef3ccf
ldumay <dumay.famille@gmail.com>
2023-03-08 22:43:48 +0100

    Create ubuntu_install_php.sh

---
+++ ubuntu_install_php.sh
@@ -0,0 +1,58 @@
+phpVersion="Lastest"
+separateLine="- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "[Script - PHP - $phpVersion] Installation de PHP $phpVersion sur Ubuntu 22.04"
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Mise à jour des dépôts"
+echo $separateLine
+sudo apt update && apt upgrade
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Mise à jour des dépôts"
+echo $separateLine
+sudo apt update && apt upgrade
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Installation des prérequis"
+echo $separateLine
+sudo apt install curl wget git tree nano zip unzip libpcre3
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Nettoyage des précédentes versions de PHP"
+echo $separateLine
+sudo apt-get purge 'php*'
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Installation de PHP $phpVersion"
+echo $separateLine
+sudo apt -y install php
+sudo apt -y install php-amqp
+sudo apt -y install php-apcu
+sudo apt -y install php-bcmath
+sudo apt -y install php-cli
+sudo apt -y install php-common
+sudo apt -y install php-curl
+sudo apt -y install php-ctype
+sudo apt -y install php-dom
+sudo apt -y install php-fpm
+sudo apt -y install php-gd
+sudo apt -y install php-gmp
+sudo apt -y install php-intl
+sudo apt -y install php-json
+sudo apt -y install php-mbstring
+sudo apt -y install php-mysql 
+sudo apt -y install php-opcache
+sudo apt -y install php-pdo
+sudo apt -y install php-pgsql
+sudo apt -y install php-readline
+sudo apt -y install php-redis
+sudo apt -y install php-simplexml
+sudo apt -y install php-soap
+sudo apt -y install php-sqlite3
+sudo apt -y install php-tokenizer
+sudo apt -y install php-xdebug
+sudo apt -y install php-xml 
+sudo apt -y install php-zip
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Installation de $phpVersion terminée"
+echo $separateLine
+php -v
+echo $separateLine
+echo "[Script - PHP - $phpVersion] Installation de Composer"
+echo $separateLine
+sudo apt install composer
